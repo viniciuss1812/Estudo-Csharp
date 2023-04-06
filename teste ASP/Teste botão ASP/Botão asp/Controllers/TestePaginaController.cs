5aa23c2dca49c7a0c38bd1152bf0aed1adffd22c
Nitierk <115711742+Mario-Cesar1@users.noreply.github.com>
2023-04-06 16:18:12 -0300

    Botões ASP

---
+++ teste ASP/Teste botão ASP/Botão asp/Controllers/TestePaginaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Botão_asp.Controllers
+{
+    public class TestePaginaController : Controller
+    {
+        public IActionResult Index(string email)
+        {
+            return View();
+        }
+    }
+}
